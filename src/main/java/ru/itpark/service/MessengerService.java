df5d7631ced0eba378769d90f4e0829bee92ef6d
student <student@localhost>
2019-02-02 04:00:05 +0300

    Messenger Service for initials generation + auto tests

---
+++ src/main/java/ru/itpark/service/MessengerService.java
@@ -0,0 +1,9 @@
+package ru.itpark.service;
+
+public class MessengerService {
+    public String initials (String name) {
+        String[] parts = name.split(" ");
+
+        return parts[0].substring(0,1) + parts[1].substring(0,1);
+    }
+}
---
+++ src/test/java/ru/itpark/service/MessengerTest.java
@@ -0,0 +1,19 @@
+package ru.itpark.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessengerServiceTest {
+
+    @Test
+    void initials() {
+        {
+            MessengerService service = new MessengerService();
+            String actual = service.initials("Vasiliy Petrov");
+            String expected = "VP";
+
+            assertEquals(expected, actual);
+        }
+    }
+}
